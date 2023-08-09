0114886bdf596b9cf7a0404b799280e4013fe087
hurricane1988 <hurricane_1988@163.com>
2023-08-09 14:10:08 +0800

    add port-scan tool

---
+++ cmd/root.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2023 QKP Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/wonderivan/logger"
+	"os"
+
+	"kube-tools/pkg/scan_port"
+)
+
+// 定义根命令
+var rootCmd = &cobra.Command{
+	Use:   "kube-tools",
+	Short: "kube-tools is a tool contains some thing",
+	Long:  "kube-tools is a tool contains some thing",
+	// 定义根命令执行函数
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("welcome to kube-tools")
+	},
+}
+
+// Execute 定义cobra执行器
+func Execute() {
+	// 注册子端口扫描子命令到root根命令
+	rootCmd.AddCommand(scan_port.ExecutePortScan())
+	if err := rootCmd.Execute(); err != nil {
+		logger.Error(err)
+		os.Exit(1)
+	}
+}
--- pkg/scan_port/scan_port.go
+++ pkg/scan_port/scan_port.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package main
+package scan_port
 
 import (
 	"fmt"
@@ -28,27 +28,20 @@ import (
 	"time"
 )
 
-func main() {
-	Execute()
-}
-
-// 定义全局变量
-var rootCmd = &cobra.Command{
-	Use:   "scan-port",
-	Short: "scan-port is a tool to scan ports",
-	Long:  "scan-port is a tool to scan ports",
-	Run:   runnerScan,
-}
-
-// Execute 定义cobra执行器
-func Execute() {
-	rootCmd.Flags().StringP("host", "H", "localhost", "host to scan")
-	rootCmd.Flags().StringP("protocol", "T", "tcp", "protocol to scan")
-	rootCmd.Flags().StringP("ports", "P", "22", "ports to scan")
-	if err := rootCmd.Execute(); err != nil {
-		logger.Error(err)
-		os.Exit(1)
+// ExecutePortScan 定义端口扫描执行器
+func ExecutePortScan() *cobra.Command {
+	// portsScan 定义端口扫描子命令
+	var portsScan = &cobra.Command{
+		Use:   "scan",
+		Short: "scan ports",
+		Long:  "scan ports",
+		Run:   runnerScan,
 	}
+	// 初始化命令
+	portsScan.Flags().StringP("host", "H", "localhost", "host to scan")
+	portsScan.Flags().StringP("protocol", "T", "tcp", "protocol to scan")
+	portsScan.Flags().StringP("ports", "P", "22", "ports to scan")
+	return portsScan
 }
 
 // runner 扫描端口
@@ -93,10 +86,11 @@ func scanner(host string, protocol string, ports []int) {
 	wg.Wait()
 }
 
+// 扫描端口
 func scanPort(host string, protocol string, port int) {
 	conn, err := net.DialTimeout(protocol, fmt.Sprintf("%s:%d", host, port), 2*time.Second)
 	if err != nil {
-		logger.Info("Port %d is close", port)
+		logger.Warn("Port %d is close", port)
 	} else {
 		logger.Info("Port %d is open", port)
 		conn.Close()
